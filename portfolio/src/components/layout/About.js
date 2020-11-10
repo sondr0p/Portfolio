c56e5ab681d5062f823ba7e2b6eaffbefb2f19f4
Sidney Son <psndnb@gmail.com>
2020-11-09 17:17:49 -0800

    adding stuff to about

--- portfolio/src/components/layout/About.js
+++ portfolio/src/components/layout/About.js
@@ -5,6 +5,13 @@ import twitter from '../../../public/twitter.png';
 import linkedin from '../../../public/linkedin.png';
 import github from '../../../public/github.png';
 import email from '../../../public/email.png';
+import csharp from '../../../public/csharp.png';
+import python from '../../../public/python.png';
+import javascript from '../../../public/javascript.png';
+import sql from '../../../public/sql.png';
+import dotnet from '../../../public/dotnet.png';
+import django from '../../../public/django.png';
+import react from '../../../public/react.png';
 
 export default class About extends Component {
     render() {
@@ -33,13 +40,42 @@ export default class About extends Component {
                             </div>
                             <div className="col-sm-8" data-aos="fade-left">
                                 <p className="about-desc">
-                                    I am a full stack software engineer based in Granada Hills, CA.
-                                    Recently, I have been using .NET or Django with React for web based work but fall back to Python for simpler
-                                    projects.
-                                    Outside of coding, some my interests include working out, following the NBA, or going out to try new
-                                    places/food!
-                                        If you would like to contact me, feel free to send me an email or give me a call with the number given on my <a href={resume}>resume</a>.
-                                    </p>
+                                    {">"} I am a full stack software engineer based in Los Angeles, CA.
+                                </p>
+                                <p className="about-desc">
+                                    {">"} Outside of coding, some my interests include working out, following the NBA/NFL, or going out to try new places to eat.
+                                </p>
+                                <p className="about-desc">
+                                    {">"} If you would like to contact me, feel free to send me an email or give me a call with the number given on my <a href={resume}>resume</a>.
+                                </p>
+
+                                <h5 className="text-center">Frameworks/Technologies I've used</h5>
+                                <ul className="about-tech nav justify-content-center">
+                                    <li>
+                                        <img className="about-tech-icons" src={dotnet} alt="" />
+                                    </li>
+                                    <li>
+                                        <img className="about-tech-icons" src={react} alt="" />
+                                    </li>
+                                    <li>
+                                        <img className="about-tech-icons" src={django} alt="" />
+                                    </li>
+                                </ul>
+                                <h5 className="text-center">Languages I've used</h5>
+                                <ul className="about-tech nav justify-content-center">
+                                    <li>
+                                        <img className="about-tech-icons" src={csharp} alt="" />
+                                    </li>
+                                    <li>
+                                        <img className="about-tech-icons" src={python} alt="" />
+                                    </li>
+                                    <li>
+                                        <img className="about-tech-icons" src={javascript} alt="" />
+                                    </li>
+                                    <li>
+                                        <img className="about-tech-icons" src={sql} alt="" />
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </div>
